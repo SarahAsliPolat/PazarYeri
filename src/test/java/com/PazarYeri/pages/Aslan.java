a68a67208c72fe3b5d0355db83c1c51d3dab4c3d
AslanElviraElza <orientalion87@gmail.com>
2020-11-01 22:48:28 -0600

    Aslan Branch

--- src/test/java/com/PazarYeri/pages/Aslan.java
+++ src/test/java/com/PazarYeri/pages/Aslan.java
@@ -4,6 +4,6 @@ public class Aslan {
     public static void main(String[] args) {
         System.out.println("Deneme");
         System.out.println("Aslan");
-
+        System.out.println("Ablanin hatrina");
     }
 }
