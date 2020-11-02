1cdf535ae18aae9176715fc67e5c1d2cc52c2e08
AishaAydin <ayseydin13@gmail.com>
2020-11-02 00:00:44 -0500

    hey git

--- src/test/java/com/PazarYeri/pages/ArhmColak.java
+++ src/test/java/com/PazarYeri/pages/ArhmColak.java
@@ -3,5 +3,6 @@ package com.PazarYeri.pages;
 public class ArhmColak {
     public static void main(String[] args) {
         System.out.println("Hi there");
+        System.out.println("Hello there");
     }
 }
