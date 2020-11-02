adcc227b31b3ff15aaef234b0e7a328483975c40
AishaAydin <ayseydin13@gmail.com>
2020-11-01 23:56:26 -0500

    hey git

--- src/test/java/com/PazarYeri/pages/Aisha.java
+++ src/test/java/com/PazarYeri/pages/Aisha.java
@@ -2,6 +2,8 @@ package com.PazarYeri.pages;
 
 public class Aisha {
     public static void main(String[] args) {
+
         System.out.println("Today is Sunday");
+        System.out.println("Hey git");
     }
 }
