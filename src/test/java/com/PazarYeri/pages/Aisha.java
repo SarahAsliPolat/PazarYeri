af08373deb8d20140749343ef640aa64dcd8da2c
AishaAydin <ayseydin13@gmail.com>
2021-02-21 22:45:54 -0500

    today's date february 21

--- src/test/java/com/PazarYeri/pages/Aisha.java
+++ src/test/java/com/PazarYeri/pages/Aisha.java
@@ -3,5 +3,7 @@ package com.PazarYeri.pages;
 public class Aisha {
     public static void main(String[] args) {
         System.out.println("Today is Sunday");
+        System.out.println("Hello world");
+        System.out.println("Todays date is 21 February");
     }
 }
